a5a4c10d463f2617b3deab51817ec74895298140
Yannick Fricke <yannickfricke@googlemail.com>
2020-04-10 17:20:48 +0200

    Added the global style component

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -1,6 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createGlobalStyle } from 'styled-components';
 
 import App from './app/app';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const GlobalStyle = createGlobalStyle`
+    body {
+      margin: 0;
+      padding: 0;
+      font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
+    }
+`;
+
+ReactDOM.render(
+    <React.Fragment>
+        <GlobalStyle />
+
+        <App />
+    </React.Fragment>,
+    document.getElementById('root'),
+);
