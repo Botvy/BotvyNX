89b740c80de7ef84c48d3daf998749c8d5d02f39
Yannick Fricke <yannickfricke@googlemail.com>
2020-04-10 16:50:01 +0200

    Added the basic frontend application

---
+++ apps/frontend/src/app/app.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledApp = styled.div`
+`;
+
+export const App = () => {
+    return (
+        <StyledApp>
+            Hello world!
+        </StyledApp>
+    );
+};
+
+export default App;
