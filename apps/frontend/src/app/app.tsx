79f9a8ff91dfaefd7c44c0112a7fa865de273baa
Yannick Fricke <yannickfricke@googlemail.com>
2020-04-13 02:23:49 +0200

    Use the new layout
    Updated the app to use the new components from the last commit

--- apps/frontend/src/app/app.tsx
+++ apps/frontend/src/app/app.tsx
@@ -1,10 +1,34 @@
 import React from 'react';
+import { LeftSidebar, RigthSidebar } from './layout/Sidebar';
+import Header from './layout/Header';
+import Footer from './layout/Footer';
 import styled from 'styled-components';
 
-const StyledApp = styled.div``;
+export const StyledApp = styled.div`
+    width: 100vw;
+`;
+
+export const Wrapper = styled.div`
+    display: inline-flex;
+`;
+
+export const StyledContent = styled.div`
+    width: 60vw;
+    float: left;
+`;
 
 export const App = () => {
-    return <StyledApp>Hello world!</StyledApp>;
+    return <StyledApp id="app">
+        <Header/>
+        <Wrapper>
+            <LeftSidebar/>
+            <StyledContent id="content">
+                Content
+            </StyledContent>
+            <RigthSidebar/>
+        </Wrapper>
+        <Footer/>
+    </StyledApp>;
 };
 
 export default App;
