23593cbbdd7a6db9386436a868e540033b5f9602
Yannick Fricke <yannickfricke@googlemail.com>
2020-04-16 00:12:33 +0200

    Updated IPlugin and added the IPluginDescriptor
    Added the getComponentsForSection function for the IPlugin interface
    Also added the IPluginDescriptor interface

--- libs/framework/plugins/src/lib/IPlugin.ts
+++ libs/framework/plugins/src/lib/IPlugin.ts
@@ -1,6 +1,7 @@
 /**
  * Defines a basic plugin
  */
+
 export interface IPlugin {
     /**
      * The identifier of the plugin
@@ -16,4 +17,17 @@ export interface IPlugin {
      * The version of the plugin
      */
     version: string;
+
+    /**
+     * Returns the components for a section
+     * @param section The section to get the components for
+     */
+    getComponentsForSection: (section: string) => JSX.Element;
+}
+
+export interface IPluginDescriptor extends IPlugin {
+    /**
+     * The main entry path to the plugin
+     */
+    main: string;
 }
